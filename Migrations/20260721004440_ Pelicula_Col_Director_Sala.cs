ffebcfac4477611b842f071651d2e673a7b3e227
Angelfar <afar12101210@gmail.com>
2026-08-12 22:12:50 -0600

    Primer Push

---
+++ Migrations/20260721004440_ Pelicula_Col_Director_Sala.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCPeliculas.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pelicula_Col_Director_Sala : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Director",
+                table: "Peliculas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Director",
+                table: "Peliculas");
+
+            migrationBuilder.DropColumn(
+                name: "Sala",
+                table: "Peliculas");
+        }
+    }
+}
---
+++ Models/Pelicula.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCPeliculas.Models
+{
+    public class Pelicula
+    {
+        public int Id { get; set; }
+        [StringLength(250)]
+        [Required]
+        [Display(Name = "Título")]
+        public string Titulo { get; set; }
+        [Display(Name = "Fecha de lanzamiento")]
+        [DataType(DataType.Date)]
+        public DateTime FechaLanzamiento { get; set; }
+        [Column(TypeName = "money")]
+        [Required]
+        public decimal Precio { get; set; }
+        [StringLength(15)]
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public int GeneroId { get; set; } // <- Llave foranea
+        [Display(Name = "Género")]
+        public Genero? Genero { get; set; } // <- Propiedad de navegacion
+    }
+    public class Genero
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
+
